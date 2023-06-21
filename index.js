bb2e9bd39d5c582a30cdfe91e601ce182a2c76a0
iamcal77 <+>
2023-06-21 13:21:17 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+// Function to move the DODGER to the left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  // Function to move the DODGER to the right
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  // Event listener for the left arrow key
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+  // Event listener for the right arrow key
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
